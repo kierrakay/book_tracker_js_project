208429c2ba5d40b01168088da95d7231cb9ae1c5
kierra <51520037+kierrakay@users.noreply.github.com>
2020-07-18 17:22:24 -0400

    a new USER can input their name and be greeted with a greeting message upon submit

---
+++ js_project_frontend/src/user.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    User.createUser();
+})
+
+//Params associated with a user
+class User {
+    constructor(user) {
+        this.id = user.id;
+        this.name = user.name;
+        this.books = user.books;
+     
+    }
+//POST fetch for creating a User
+    static createUser(user) {
+        let newUserForm = document.getElementById('new-user-form')
+        newUserForm.addEventListener('submit', function(e){
+            e.preventDefault();
+            fetch('http://localhost:3000/api/v1/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'Accept' : 'application/json'
+                },
+                body: JSON.stringify({
+                    user: {
+                        name: e.target.children[1].value
+                    }
+                })
+            })
+                .then(res => {
+                    if (!res.ok) {
+                        throw new Error(); // Will take you to the `catch` below
+                    }
+                    return res.json();
+                })
+                .then (user => {
+                    let newUser = new User(user)
+                    console.log(user)
+                    newUser.displayUser();
+                })
+                .catch(error => {
+                    console.error('User class Error', error)
+                })
+            })
+    }
+
+    displayUser() {
+        let body = document.getElementById('container');
+        body.innerHTML = ''
+        let div = document.createElement('div');
+        div.setAttribute('class', 'user-greeting');
+        let bc = document.getElementById('books-container')
+        bc.classList.remove('hidden')
+        let Greeting = document.createElement('p');
+        Greeting.innerHTML = `<h1>Hey ${this.name}! What did you read this week?</h1>`
+        div.appendChild(Greeting);
+        body.appendChild(div);
+    }
+
+}
+        
+   
